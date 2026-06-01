9cdedfc019d39865f66287d0900c07cba550ce2d
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-06-01 15:05:37 +0100

    Slight cleanup, made endpoint more specific. changed to use h2 database in development stage.

--- PriceHistory/src/main/java/com/mthree/TradingPlatform/controller/PriceHistoryController.java
+++ PriceHistory/src/main/java/com/mthree/TradingPlatform/controller/PriceHistoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/price-history")
 public class PriceHistoryController {
 
     private final PriceHistoryService service;
