718ba809bbb3b341ef7bf8493c7692cb008891fb
Avantee Singh <avantee280209@gmail.com>
2026-06-01 18:39:07 +0530

    Remove user and instrument modules from portfolio branch

--- MainModule/src/main/java/com/mthree/TradingPlatform/repository/InstrumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mthree.TradingPlatform.repository;
-
-import com.mthree.TradingPlatform.entity.Instrument;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstrumentRepository extends JpaRepository<Instrument, Long> {
-
-    boolean existsBySymbol(String symbol);
-}
