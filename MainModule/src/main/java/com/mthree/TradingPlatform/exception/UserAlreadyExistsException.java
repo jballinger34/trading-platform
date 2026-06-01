0b268c931ec41b66670e2f79972866eefcd1dde4
Avantee Singh <avantee280209@gmail.com>
2026-06-01 18:21:35 +0530

    Remove user module from instrument branch

--- MainModule/src/main/java/com/mthree/TradingPlatform/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.mthree.TradingPlatform.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- MainModule/src/main/java/com/mthree/TradingPlatform/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.mthree.TradingPlatform.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
