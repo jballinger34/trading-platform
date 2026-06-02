3e5336cdc2c83bde5876708b712126587b47848d
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-06-02 13:44:16 +0100

    Implemented CompanyDataClient

--- MainModule/src/main/java/com/mthree/TradingPlatform/client/CompanyDataClient.java
+++ MainModule/src/main/java/com/mthree/TradingPlatform/client/CompanyDataClient.java
@@ -1,5 +1,7 @@
 package com.mthree.TradingPlatform.client;
 
+import com.mthree.TradingPlatform.dto.CompanyProfileDto;
+import com.mthree.TradingPlatform.dto.FundamentalsDto;
 import com.mthree.TradingPlatform.dto.InstrumentResponseDto;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
@@ -15,6 +17,25 @@ public class CompanyDataClient {
 
     public CompanyDataClient(@Qualifier("companyDataRestClient") RestClient restClient) {
         this.restClient = restClient;
+    }
+
+    public List<FundamentalsDto> getFundamentals(List<String> symbols) {
+        if(symbols == null || symbols.isEmpty()) return List.of();
+        return restClient.get().uri(
+                uriBuilder -> uriBuilder
+                        .path("/api/v1/fundamentals")
+                        .queryParam("symbols", String.join(",", symbols))
+                        .build()
+                ).retrieve().body(new ParameterizedTypeReference<>() {});
+    }
+    public List<CompanyProfileDto> getProfiles(List<String> symbols) {
+        if(symbols == null || symbols.isEmpty()) return List.of();
+        return restClient.get().uri(
+                uriBuilder -> uriBuilder
+                        .path("/api/v1/company-profile")
+                        .queryParam("symbols", String.join(",", symbols))
+                        .build()
+        ).retrieve().body(new ParameterizedTypeReference<>() {});
     }
 
 }
