a71a0b25d2c28ce6c142ff1a91d9b532ecfbe1c0
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-06-02 10:46:29 +0100

    Implemented service methods to get profile by symbol and get batch of profiles by list of symbols. same for fundamentals.

--- CompanyData/src/main/java/com/mthree/TradingPlatform/service/CompanyDataService.java
+++ CompanyData/src/main/java/com/mthree/TradingPlatform/service/CompanyDataService.java
@@ -1,5 +1,6 @@
 package com.mthree.TradingPlatform.service;
 
+import com.mthree.TradingPlatform.CompanyDataApp;
 import com.mthree.TradingPlatform.domain.model.Company;
 import com.mthree.TradingPlatform.domain.model.Fundamentals;
 import com.mthree.TradingPlatform.dto.CompanyProfileDto;
@@ -46,9 +47,23 @@ public class CompanyDataService {
         fundamentalsRepository.save(fundamentals);
     }
 
-    //need to implement methods to get profile by symbol
-    //or get batch of profiles by list of symbols
+    public CompanyProfileDto getProfileBySymbol(String symbol){
+        Company company = companyRepository.findById(symbol).orElseThrow();
+        return CompanyProfileDto.from(company);
+    }
+    public List<CompanyProfileDto> getProfilesBySymbols(List<String> symbols){
+        List<Company> companies = companyRepository.findAllById(symbols);
+        return companies.stream().map(CompanyProfileDto::from).toList();
+    }
+    public FundamentalsDto getFundamentalsBySymbol(String symbol){
+        Fundamentals fundamentals = fundamentalsRepository.findById(symbol).orElseThrow();
+        return FundamentalsDto.from(fundamentals);
+    }
+    public List<FundamentalsDto> getFundamentalsBySymbols(List<String> symbols){
+        List<Fundamentals> allFundamentals = fundamentalsRepository.findAllById(symbols);
+        return allFundamentals.stream().map(FundamentalsDto::from).toList();
+    }
 
-    //same for fundamentals
+
 
 }
