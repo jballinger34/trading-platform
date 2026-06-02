06a289495a9227b428161fad5b85dea79129636c
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-06-02 10:55:18 +0100

    Cleanup

--- MainModule/src/main/java/com/mthree/TradingPlatform/ScreenerApplication.java
+++ MainModule/src/main/java/com/mthree/TradingPlatform/ScreenerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TradingPlatformApplication {
+public class ScreenerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TradingPlatformApplication.class, args);
+		SpringApplication.run(ScreenerApplication.class, args);
 	}
 
 }
