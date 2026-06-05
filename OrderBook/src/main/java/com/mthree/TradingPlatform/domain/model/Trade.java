9480af5aa0f8a7990a9c6229052c800d37fb57e3
Jamie Ballinger <ballingerjamie04@gmail.com>
2026-06-05 12:28:48 +0100

    Added orderIds back to Trade. OrderService will need these to keep track of state of orders per user.

--- OrderBook/src/main/java/com/mthree/TradingPlatform/domain/model/Trade.java
+++ OrderBook/src/main/java/com/mthree/TradingPlatform/domain/model/Trade.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
-public record Trade(UUID tradeId, UUID buyerUserId, UUID sellerUserId, String symbol, long quantity, BigDecimal price) {
+public record Trade(UUID tradeId, UUID buyOrderId, UUID sellOrderId, UUID buyerUserId, UUID sellerUserId, String symbol, long quantity, BigDecimal price) {
     public static Trade create(Order bid, Order ask, String symbol, long quantity, BigDecimal price){
-        return new Trade(UUID.randomUUID(), bid.getUserId(), ask.getUserId(), symbol, quantity, price);
+        return new Trade(UUID.randomUUID(),bid.getOrderId(), ask.getOrderId(), bid.getUserId(), ask.getUserId(), symbol, quantity, price);
     }
 }
